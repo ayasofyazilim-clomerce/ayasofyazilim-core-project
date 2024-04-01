cb35c5c40dad0f35d9e19c644f219a8114a8c491
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-04-01 16:31:49 +0300

    feat: add sidebar menus

--- apps/web/src/app/[lang]/(main)/layout.tsx
+++ apps/web/src/app/[lang]/(main)/layout.tsx
@@ -1,16 +1,51 @@
 'use client';
+import {
+    SquareStack,
+    User,
+} from 'lucide-react';
+
 import { JSXElementConstructor, ReactElement, ReactNode } from "react";
-import Mainlayout from "@repo/ayasofyazilim-ui/templates/mainlayout";
+import Mainlayout, { mainLayoutProps } from "@repo/ayasofyazilim-ui/templates/mainlayout";
 import "./../../globals.css";
 
-export default async function Layout({
-    children,
-}: {
+type LayoutProps = {
     children: ReactElement<any, string | JSXElementConstructor<any>>;
-}) {
+}
+
+type MenuProps = {
+    href: string;
+    icon: ReactNode;
+    label: string;
+    name: string;
+    submenu?: Submenu[];
+};
+
+type Submenu = {
+    href: string;
+    icon: ReactNode;
+    name: string;
+};
+
+export default async function Layout({ children }: LayoutProps) {
+    const exampleMenus: MenuProps[] = [
+        {
+            label: 'Pages',
+            name: 'Profile',
+            icon: <User size={15} className="mr-2" />,
+            href: 'profile',
+        },
+        {
+            label: 'Pages',
+            name: 'Dashboard',
+            icon: <SquareStack size={15} className="mr-2" />,
+            href: 'dashboard',
+        },
+    ];
     return (
-        <Mainlayout logo="https://github.com/ayasofyazilim-clomerce.png" title="ayaasofya">
-            {children}
+        <Mainlayout logo="https://github.com/ayasofyazilim-clomerce.png" title="ayaasofya" menus={exampleMenus}>
+            <>
+                {children}
+            </>
         </Mainlayout>
     );
 }
