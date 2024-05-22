4ec2021a0475f1b56670b6bc05a26a85956a6956
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-05-22 08:11:20 +0300

    chore:public_web restore

--- apps/public_web/src/app/page.tsx
+++ apps/public_web/src/app/page.tsx
@@ -1,40 +1,6 @@
-"use server";
-import { redirect } from "next/navigation";
-import { auth, signIn, signOut } from "auth";
+// import { redirect } from "next/navigation";
 
-export default async function Page(): Promise<JSX.Element> {
-  const session = await auth();
-
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      Yeahh
-      <form
-        action={async () => {
-          "use server";
-          await signIn("credentials", {
-            username: "eren12",
-            password: "123Aa!",
-            redirect: false,
-          });
-          redirect("/");
-        }}
-      >
-        <button type="submit">Sign in</button>
-        {session ? (
-          <p>signed in {JSON.stringify(session)}</p>
-        ) : (
-          <p>Not signed in</p>
-        )}
-      </form>
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirect: false });
-          redirect("/");
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
-    </main>
-  );
+export default function Page(): JSX.Element {
+  return <>TEEEST</>;
+  // redirect("/tr/projects");
 }
