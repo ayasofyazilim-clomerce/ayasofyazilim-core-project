bb5ce7eb20582b73803ccd95c748b85f67129c9a
Eren <eren.odaci@ayasofyazilim.com>
2024-05-29 15:20:37 +0300

    fix: infinity loop in api request

---
+++ apps/web/src/app/api/route.ts
@@ -0,0 +1,18 @@
+"use server";
+import { AccountServiceClient } from "@ayasofyazilim/saas/AccountService";
+
+const appClient = new AccountServiceClient({
+  BASE: process.env.BASE_URL,
+});
+
+export async function GET(request: Request) {
+  // get search params from request
+  const url = new URL(request.url);
+  const lang = url.searchParams.get("lang") || "en";
+  // Use the client instance to make the API call
+  const response =
+    await appClient.abpApplicationLocalization.getApiAbpApplicationLocalization(
+      { cultureName: lang }
+    );
+  return new Response(JSON.stringify(response));
+}
