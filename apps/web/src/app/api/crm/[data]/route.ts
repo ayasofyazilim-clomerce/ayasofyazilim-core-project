b63681d1139d9f07eef02476cd3e07414c693360
yusualhashash <yusufalhashash2018@gmail.com>
2024-08-20 16:37:01 +0300

    refactor: add all function of backend for CRM page

--- apps/web/src/app/api/crm/[data]/route.ts
+++ apps/web/src/app/api/crm/[data]/route.ts
@@ -1,384 +1,86 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument -- TODO: we need to fix this*/
 import type { Volo_Abp_Http_RemoteServiceErrorResponse } from "@ayasofyazilim/saas/AccountService";
 import type { NextRequest } from "next/server";
-import {
-  getIdentityServiceClient,
-  getMerchantServiceClient,
-  getSaasServiceClient,
-} from "src/lib";
+import { getCRMServiceClient } from "src/lib";
 import type { Clients } from "../../util";
 import { errorResponse, isApiError } from "../../util";
 
 const clients: Clients = {
   merchants: async () => {
-    const client = await getMerchantServiceClient();
-    const merchant = client.organization;
-
+    const client = await getCRMServiceClient();
+    const merchant = client.merchant;
     return {
-      get: async () => {
-        const getDetails =
-          await merchant.getApiMerchantServiceOrganizationsDetail();
-
-        return getDetails.items?.map((organization) => {
-          const contactInfo = organization.contactInformation || {};
-          const telephone = contactInfo.telephones?.[0] || {};
-          const address = contactInfo.addresses?.[0] || {};
-          const email = contactInfo.emails?.[0] || {};
-          const productGroup = organization.productGroups?.[0] || {};
-
-          return {
-            id: organization.id || "",
-            name: organization.name || "",
-            taxpayerId: organization.taxpayerId || "",
-            legalStatusCode: organization.legalStatusCode || "",
-            customerNumber: organization.customerNumber || "",
-            areaCode: telephone.areaCode || "",
-            localNumber: telephone.localNumber || "",
-            ituCountryCode: telephone.ituCountryCode || "",
-            primaryFlag: telephone.primaryFlag || false,
-            telephoneTypeCode: telephone.typeCode || 0,
-            addressLine: address.addressLine || "",
-            city: address.city || "",
-            terriority: address.terriority || "",
-            postalCode: address.postalCode || "",
-            country: address.country || "",
-            fullAddress: address.fullAddress || "",
-            addressPrimaryFlag: address.primaryFlag || false,
-            addressTypeCode: address.typeCode || 0,
-            emailAddress: email.emailAddress || "",
-            emailPrimaryFlag: email.primaryFlag || false,
-            emailTypeCode: email.typeCode || 0,
-            productName: productGroup.name || "",
-            vatRate: productGroup.vatRate || 0,
-            productCode: productGroup.productCode || "",
-            isActive: productGroup.isActive || false,
-          };
-        });
-      },
-      post: async (requestBody: any) =>
-        merchant.postApiMerchantServiceOrganizations({
-          requestBody,
-          entityInformationTypeId: "e5f7f9e0-ceee-71f6-7b93-3a136c155b82",
+      get: (page: number, maxResultCount: number) =>
+        merchant.getApiCrmServiceMerchants({
+          maxResultCount: maxResultCount || 10,
+          skipCount: page * 10,
         }),
-      put: async (requestBody: {
-        id: string;
-        requestBody: {
-          name: string;
-          taxpayerId: string;
-          legalStatusCode: string;
-          customerNumber: string;
-          areaCode: string;
-          localNumber: string;
-          ituCountryCode: string;
-          primaryFlag: boolean;
-          telephoneTypeCode: number;
-          addressLine: string;
-          city: string;
-          terriority: string;
-          postalCode: string;
-          country: string;
-          fullAddress: string;
-          addressPrimaryFlag: boolean;
-          addressTypeCode: number;
-          emailAddress: string;
-          emailPrimaryFlag: boolean;
-          emailTypeCode: number;
-          productName: string;
-          vatRate: number;
-          productCode: string;
-          isActive: boolean;
-        };
-      }) => {
-        const currentDetails =
-          await merchant.getApiMerchantServiceOrganizationsDetailById({
-            id: requestBody.id,
-          });
-
-        const updatedDetails = {
-          ...currentDetails,
-          name: requestBody.requestBody.name,
-          taxpayerId: requestBody.requestBody.taxpayerId,
-          legalStatusCode: requestBody.requestBody.legalStatusCode,
-          customerNumber: requestBody.requestBody.customerNumber,
-          contactInformation: {
-            ...currentDetails.contactInformation,
-            telephones: [
-              {
-                ...currentDetails.contactInformation?.telephones?.[0],
-                areaCode: requestBody.requestBody.areaCode,
-                localNumber: requestBody.requestBody.localNumber,
-                ituCountryCode: requestBody.requestBody.ituCountryCode,
-                primaryFlag: requestBody.requestBody.primaryFlag,
-                typeCode: requestBody.requestBody.telephoneTypeCode,
-              },
-            ],
-            addresses: [
-              {
-                ...currentDetails.contactInformation?.addresses?.[0],
-                addressLine: requestBody.requestBody.addressLine,
-                city: requestBody.requestBody.city,
-                terriority: requestBody.requestBody.terriority,
-                postalCode: requestBody.requestBody.postalCode,
-                country: requestBody.requestBody.country,
-                fullAddress: requestBody.requestBody.fullAddress,
-                primaryFlag: requestBody.requestBody.addressPrimaryFlag,
-                typeCode: requestBody.requestBody.addressTypeCode,
-              },
-            ],
-            emails: [
-              {
-                ...currentDetails.contactInformation?.emails?.[0],
-                emailAddress: requestBody.requestBody.emailAddress,
-                primaryFlag: requestBody.requestBody.emailPrimaryFlag,
-                typeCode: requestBody.requestBody.emailTypeCode,
-              },
-            ],
-          },
-          productGroups: [
-            {
-              ...currentDetails.productGroups?.[0],
-              name: requestBody.requestBody.productName,
-              vatRate: requestBody.requestBody.vatRate,
-              productCode: requestBody.requestBody.productCode,
-              isActive: requestBody.requestBody.isActive,
-            },
-          ],
-        };
-
-        return merchant.putApiMerchantServiceOrganizations({
-          requestBody: updatedDetails as any,
-        });
-      },
+      post: async (requestBody: any) =>
+        merchant.postApiCrmServiceMerchantsWithComponents({ requestBody }),
       delete: async (id: string) =>
-        merchant.deleteApiMerchantServiceOrganizations({ id }),
+        merchant.deleteApiCrmServiceMerchantsWithComponentsById({ id }),
     };
   },
   refundPoints: async () => {
-    const client = await getIdentityServiceClient();
-    const user = client.user;
+    const client = await getCRMServiceClient();
+    const refundPoint = client.refundPoint;
     return {
-      get: () => [
-        {
-          Company:
-            "Travelex Döviz Ticaret Yetkili Müessese Anonim Şirketi (assigned to Mehmet Baykam)",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02822 - http://www.travelex.com.tr\nAntalya Airport Terminal 1, 1\nYenigöl\nMuratpaşa, Antalya\nTurkey",
-        },
-        {
-          Company:
-            "Travelex Döviz Ticaret Yetkili Müessese Anonim Şirketi - 5382-AYT-T1 ASD (assigned to Mehmet Baykam)",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02824 - http://www.travelex.com.tr\nAntalya Terminal 1, 1\nYenigöl\nMuratpaşa, Antalya\nTurkey",
-        },
-        {
-          Company:
-            "Travelex Ankara Döviz Ticareti Yetkili Müessese Anonim Şirketi (assigned to Mehmet Baykam)",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02827 - http://www.travelex.com.tr\nAnkara Airport, 325/3\nBalıkhisar Köyü\nAkyurt, Ankara\nTurkey",
-        },
-        {
-          Company:
-            "Travelex Döviz Ticaret Yetkili Müessese Anonim Şirketi - 5389-Kusadasi Egeport (assigned to Mehmet Baykam)",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02829 - http://www.travelex.com.tr\nKuşadası Egeport, 18\nCamikebir Mah.\nKuşadası, Aydın\nTurkey",
-        },
-        {
-          Company:
-            "Travelex Ankara Döviz Ticareti Yetkili Müessese Anonim Şirketi - 5371-IZM-LSD (assigned to Mehmet Baykam)",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02830 - http://www.travelex.com.tr\nİzmir Airport, 1\nDokuzeylül(9 Eylül) Mah.\nGaziemir, İzmir\nTurkey",
-        },
-        {
-          Company:
-            "Travelex Ankara Döviz Ticareti Yetkili Müessese Anonim Şirketi - 5362-ALA-LSA (assigned to Mehmet Baykam)",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02831 - http://www.travelex.com.tr\nGazipasa Airport, 1\nPaşaköy\nAlanya, Antalya\nTurkey",
-        },
-
-        {
-          Company:
-            "Global Exchange Döviz Ticaret Yetkili Müessese Anonim Şirketi",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address:
-            "TR02832 - http://www.globalexchange.com.tr/tr/istanbul\nYenigöl Mahallesi Serik Caddesi, 100/4\nYenigöl\nMuratpaşa, Antalya\nTurkey",
-        },
-      ],
+      get: (page: number, maxResultCount: number) =>
+        refundPoint.getApiCrmServiceRefundPoints({
+          maxResultCount: maxResultCount || 10,
+          skipCount: page * 10,
+        }),
       post: async (requestBody: any) =>
-        user.postApiIdentityUsers({ requestBody }),
-      put: async ({ id, requestBody }: { id: string; requestBody: any }) =>
-        user.putApiIdentityUsersById({ id, requestBody }),
-      delete: async (id: string) => user.deleteApiIdentityUsersById({ id }),
+        refundPoint.postApiCrmServiceRefundPointsWithComponents({
+          requestBody,
+        }),
+      delete: async (id: string) =>
+        refundPoint.deleteApiCrmServiceRefundPointsWithComponentsById({ id }),
     };
   },
   customs: async () => {
-    const client = await getSaasServiceClient();
-    const edition = client.edition;
+    const client = await getCRMServiceClient();
+    const customs = client.customs;
     return {
-      get: () => [
-        {
-          Company: "340900",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR02855\nTurkey",
-        },
-        {
-          Company: "341454",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR02961\nTurkey",
-        },
-        {
-          Company: "343200",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR02963\nTurkey",
-        },
-        {
-          Company: "070200",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR02983\nTurkey",
-        },
-        {
-          Company: "060200",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR02992\nTurkey",
-        },
-        {
-          Company: "330900",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR03191\nTurkey",
-        },
-        {
-          Company: "070400",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR03471\nTurkey",
-        },
-        {
-          Company: "220200",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR03494\nTurkey",
-        },
-        {
-          Company: "221300",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR03574\nTurkey",
-        },
-        {
-          Company: "350700",
-          CustomerNumber: "-",
-          ProductGroups: "-",
-          Address: "TR03610\nTurkey",
-        },
-      ],
+      get: (page: number, maxResultCount: number) =>
+        customs.getApiCrmServiceCustoms({
+          maxResultCount: maxResultCount || 10,
+          skipCount: page * 10,
+        }),
       post: async (requestBody: any) =>
-        edition.postApiSaasEditions({ requestBody }),
-      put: async ({ id, requestBody }: { id: string; requestBody: any }) =>
-        edition.putApiSaasEditionsById({ id, requestBody }),
-      delete: async (id: string) => edition.deleteApiSaasEditionsById({ id }),
+        customs.postApiCrmServiceCustomsWithComponents({ requestBody }),
+      delete: async (id: string) =>
+        customs.deleteApiCrmServiceCustomsWithComponentsById({ id }),
     };
   },
   taxFree: async () => {
-    const client = await getSaasServiceClient();
-    const tenant = client.tenant;
+    const client = await getCRMServiceClient();
+    const taxFree = client.taxFree;
     return {
-      get: () => [],
+      get: (page: number, maxResultCount: number) =>
+        taxFree.getApiCrmServiceTaxFrees({
+          maxResultCount: maxResultCount || 10,
+          skipCount: page * 10,
+        }),
       post: async (requestBody: any) =>
-        tenant.postApiSaasTenants({ requestBody }),
-      put: async ({ id, requestBody }: { id: string; requestBody: any }) =>
-        tenant.putApiSaasTenantsById({ id, requestBody }),
-      delete: async (id: string) => tenant.deleteApiSaasTenantsById({ id }),
+        taxFree.postApiCrmServiceTaxFreesWithComponents({ requestBody }),
+      delete: async (id: string) =>
+        taxFree.deleteApiCrmServiceTaxFreesWithComponentsById({ id }),
     };
   },
   taxOffices: async () => {
-    const client = await getSaasServiceClient();
-    const tenant = client.tenant;
+    const client = await getCRMServiceClient();
+    const taxOffice = client.taxOffice;
     return {
-      get: () => [
-        {
-          Company: "Düzce Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "81260",
-          ProductGroups: "-",
-          Address: "TR14341\nBond Street 233\nTurkey",
-        },
-        {
-          Company: "Osmaniye Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "80201",
-          ProductGroups: "-",
-          Address: "TR14340\nBond Street 232\nTurkey",
-        },
-        {
-          Company: "Kilis Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "79201",
-          ProductGroups: "-",
-          Address: "TR14339\nBond Street 231\nTurkey",
-        },
-        {
-          Company: "Yenice Malmüdürlüğü",
-          CustomerNumber: "78105",
-          ProductGroups: "-",
-          Address: "TR02093\nBond Street 230\nTurkey",
-        },
-        {
-          Company: "Ovacık Malmüdürlüğü",
-          CustomerNumber: "78103",
-          ProductGroups: "-",
-          Address: "TR02699\nBond Street 229\nTurkey",
-        },
-        {
-          Company: "Karabük Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "78201",
-          ProductGroups: "-",
-          Address: "TR14338\nBond Street 228\nTurkey",
-        },
-        {
-          Company: "Yalova Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "77201",
-          ProductGroups: "-",
-          Address: "TR14337\nBond Street 227\nTurkey",
-        },
-        {
-          Company: "Iğdır Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "76201",
-          ProductGroups: "-",
-          Address: "TR14336\nBond Street 226\nTurkey",
-        },
-        {
-          Company: "Ardahan Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "75201",
-          ProductGroups: "-",
-          Address: "TR14335\nBond Street 225\nTurkey",
-        },
-        {
-          Company: "Bartın Vergi Dairesi Müdürlüğü",
-          CustomerNumber: "74260",
-          ProductGroups: "-",
-          Address: "TR14334\nBond Street 224\nTurkey",
-        },
-      ],
+      get: (page: number, maxResultCount: number) =>
+        taxOffice.getApiCrmServiceTaxOffices({
+          maxResultCount: maxResultCount || 10,
+          skipCount: page * 10,
+        }),
       post: async (requestBody: any) =>
-        tenant.postApiSaasTenants({ requestBody }),
-      put: async ({ id, requestBody }: { id: string; requestBody: any }) =>
-        tenant.putApiSaasTenantsById({ id, requestBody }),
-      delete: async (id: string) => tenant.deleteApiSaasTenantsById({ id }),
+        taxOffice.postApiCrmServiceTaxOfficesWithComponents({ requestBody }),
+      delete: async (id: string) =>
+        taxOffice.deleteApiCrmServiceTaxOfficesWithComponentsById({ id }),
     };
   },
 };
@@ -387,13 +89,16 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { data: string } },
 ) {
+  const searchParams = request.nextUrl.searchParams;
+  const page = searchParams.get("page");
+  const maxResultCount = searchParams.get("maxResultCount");
   if (!clients[params.data]) {
     // return status 404
     return errorResponse("Invalid data type");
   }
-  const client = await clients[params.data](request);
+  const client = await clients[params.data]();
   try {
-    const data = await client.get();
+    const data = await client.get(page, maxResultCount);
     return new Response(JSON.stringify(data));
   } catch (error: unknown) {
     if (isApiError(error)) {
